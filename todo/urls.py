b23307f9fb923dd78fd2516c43e4c2e2f5f9044e
Haresh Vegad <136359937+hareshvegad@users.noreply.github.com>
2023-06-14 19:34:54 +0530

    Add files via upload

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('addTask/', views.addTask,name="addTask"),
+    path('mark_as_done/<int:pk>/',views.mark_as_done,name='mark_as_done'),
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone,name='mark_as_undone'),
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+    path('delete_task/<int:pk>/',views.delete_task,name='delete_task'),
+]
