97ca3cb95c94b563387b1f955e0e258d8d17be6b
Haresh Vegad <136359937+hareshvegad@users.noreply.github.com>
2023-10-10 11:04:39 +0530

    Update settings.py as ALLOWED_HOSTS = ['.vercel.app']

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0i+$o7(v4c2st9znvaln+%wd#i^9#%n=(@_u&w%a2kscc^dx%f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
